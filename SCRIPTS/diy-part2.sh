73436b53677d19508e5cbf4438ccbc1d038a7419
stroll101 <85207301+stroll101@users.noreply.github.com>
2021-06-21 09:32:45 +0800

    Update diy-part2.sh

--- SCRIPTS/diy-part2.sh
+++ SCRIPTS/diy-part2.sh
@@ -20,6 +20,11 @@ pushd package/community
 # Add Lienol's Packages
 git clone --depth=1 https://github.com/Lienol/openwrt-package
 
+# Add luci-app-ssr-plus
+pushd package/lean
+git clone --depth=1 https://github.com/fw876/helloworld
+popd
+
 # Add luci-app-passwall
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 
