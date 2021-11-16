3f950c4cc2bf4e0c11b35755dbada6c643459358
stroll101 <85207301+stroll101@users.noreply.github.com>
2021-11-16 14:52:40 +0800

    Update 2_clone_source.sh

--- 2_clone_source.sh
+++ 2_clone_source.sh
@@ -7,8 +7,8 @@ repo sync -c --no-tags --no-clone-bundle -j8
 cd friendlywrt/ && git fetch --unshallow
 git checkout `git branch -va | grep remotes/m | awk '{print $3}' | awk -F\/ '{print $2}'`
 
-[ -f target/linux/rockchip-rk3328/config-4.14 ] && sed -i '/CONFIG_CGROUPS/a\CONFIG_CGROUP_PERF=y' target/linux/rockchip-rk3328/config-4.14
-[ -f target/linux/allwinner-h5/config-4.14 ] &&    sed -i '/CONFIG_CGROUPS/a\CONFIG_CGROUP_PERF=y' target/linux/allwinner-h5/config-4.14
+[ -f target/linux/rockchip-rk3328/config-5.4 ] && sed -i '/CONFIG_CGROUPS/a\CONFIG_CGROUP_PERF=y' target/linux/rockchip-rk3328/config-5.4
+[ -f target/linux/allwinner-h5/config-5.4 ] &&    sed -i '/CONFIG_CGROUPS/a\CONFIG_CGROUP_PERF=y' target/linux/allwinner-h5/config-5.4
 git add . && git commit -m 'reset'
 
 cd ../ && find device/ -name distfeeds.conf -delete
