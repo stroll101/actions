731be363926b518e8edfe8f5fc28736b0c1d8887
stroll101 <85207301+stroll101@users.noreply.github.com>
2022-03-16 17:30:22 +0800

    Update diy-part2.sh

--- SCRIPTS/diy-part2.sh
+++ SCRIPTS/diy-part2.sh
@@ -23,7 +23,11 @@ mkdir package/community
 pushd package/community
 
 # Add Lienol's Packages
-git clone --depth=1 https://github.com/Lienol/openwrt-package
+#git clone --depth=1 https://github.com/Lienol/openwrt-package
+#rm -rf ../lean/luci-app-kodexplorer
+
+# Add kenzo small-package
+git clone --depth=1 https://github.com/kenzok8/small-package
 rm -rf ../lean/luci-app-kodexplorer
 
 # Add ucode (提交不完整)
