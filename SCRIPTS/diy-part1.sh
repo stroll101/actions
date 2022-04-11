231172766dfc3f22b1e43c15725d68dad5fc7c8b
stroll101 <85207301+stroll101@users.noreply.github.com>
2022-04-11 08:54:59 +0800

    Update diy-part1.sh

--- SCRIPTS/diy-part1.sh
+++ SCRIPTS/diy-part1.sh
@@ -14,7 +14,7 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 
 # Add luci-theme-argon
