66338b80ec620b94865a2feb4781c88d7a066613
stroll101 <85207301+stroll101@users.noreply.github.com>
2022-04-11 21:28:36 +0800

    Create diy-part2e.sh

---
+++ SCRIPTS/diy-part2e.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Add luci-app-ssr-plus
+pushd package/lean
+git clone --depth=1 https://github.com/fw876/helloworld
+popd
+
+# Clone community packages to package/community
+mkdir package/community
+pushd package/community
+
+# Add luci-app-oaf
+git clone --depth=1 https://github.com/destan19/OpenAppFilter -b oaf-3.0.1
+popd
+
+# Mod zzz-default-settings
+pushd package/emortal/default-settings/files
+sed -i '/http/d' zzz-default-settings
+sed -i '/openwrt_luci/d' zzz-default-settings
+popd
+
+# Fix mt76 wireless driver
+pushd package/kernel/mt76
+sed -i '/mt7662u_rom_patch.bin/a\\techo mt76-usb disable_usb_sg=1 > $\(1\)\/etc\/modules.d\/mt76-usb' Makefile
+popd
+
+# Change default shell to zsh
+sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
