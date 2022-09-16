2ef1848be27f864de95609b498a8d617036df6d8
stroll101 <85207301+stroll101@users.noreply.github.com>
2022-09-16 17:26:46 +0800

    Update diy-part2a.sh

--- SCRIPTS/diy-part2a.sh
+++ SCRIPTS/diy-part2a.sh
@@ -19,15 +19,3 @@ pushd package/community
 
 # Add OpenAppFilter  # Add luci-app-oaf
 git clone --depth=1 https://github.com/destan19/OpenAppFilter
-
-# Mod zzz-default-settings
-pushd package/lean/default-settings/files
-sed -i '/http/d' zzz-default-settings
-sed -i '/18.06/d' zzz-default-settings
-export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-export date_version=$(date -d "$(rdate -n -4 -p ntp.aliyun.com)" +'%Y-%m-%d')
-sed -i "s/${orig_version}/${orig_version} (${date_version})/g" zzz-default-settings
-popd
-
-# Change default shell to zsh
-sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
