559449c670b097c373b8184d07300544554466bc
stroll101 <85207301+stroll101@users.noreply.github.com>
2022-09-25 22:56:07 +0800

    Update diy-part2e.sh

--- SCRIPTS/diy-part2e.sh
+++ SCRIPTS/diy-part2e.sh
@@ -29,3 +29,12 @@ git clone --depth=1 https://github.com/destan19/OpenAppFilter
 pushd package/kernel/mt76
 sed -i '/mt7662u_rom_patch.bin/a\\techo mt76-usb disable_usb_sg=1 > $\(1\)\/etc\/modules.d\/mt76-usb' Makefile
 popd
+
+# Test kernel 5.15
+# sed -i 's/5.19/5.15/g' target/linux/x86/Makefile
+
+# Modify default IP
+# sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 替换默认主题为 luci-theme-argon
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
