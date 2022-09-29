0650c45b1e9b2e343e22f83b2ac6bad0a7444fb9
stroll101 <85207301+stroll101@users.noreply.github.com>
2022-09-29 09:29:42 +0800

    Update init-settings.sh

--- SCRIPTS/init-settings.sh
+++ SCRIPTS/init-settings.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 #=================================================
 # File name: init-settings.sh
-# Description: This script will be execute during the first boot
+# System Required: Linux
+# Version: 1.0
+# Lisence: MIT
 # Author: SuLingGG
 # Blog: https://mlapp.cn
 #=================================================
-
 # Set default theme to luci-theme-argon
 uci set luci.main.mediaurlbase='/luci-static/argon'
 
+# Disable opkg signature check
+# sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
+# 禁用ipv6前缀
+sed -i 's/^[^#].*option ula/#&/' /etc/config/network
 # Disable autostart by default for some packages
 cd /etc/rc.d
 rm -f S98udptools || true
