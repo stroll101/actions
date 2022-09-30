5f7e92d58acb891ddb03a29980a73bfdb0a4ddc0
stroll101 <85207301+stroll101@users.noreply.github.com>
2022-10-01 00:56:34 +0800

    Update diy-part2e.sh

--- SCRIPTS/diy-part2e.sh
+++ SCRIPTS/diy-part2e.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Add luci-app-ssr-plus
 pushd package/lean
