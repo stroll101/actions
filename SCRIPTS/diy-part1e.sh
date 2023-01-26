9ea0b9887cda0b73e1fb0651f59b9bbafffd4ef3
stroll101 <85207301+stroll101@users.noreply.github.com>
2023-01-26 17:45:43 +0800

    Update diy-part1e.sh

--- SCRIPTS/diy-part1e.sh
+++ SCRIPTS/diy-part1e.sh
@@ -18,6 +18,7 @@ sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.
 #sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 
 ########### 更改大雕源码（可选）###########
+sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefile
 # sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 
 # Add luci-theme-argon
